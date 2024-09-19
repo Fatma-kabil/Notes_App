a6fbac0da2dbfa8d0222119dc260a8e02e436bd0
Fatma_kabil <fatmakhaled112@gmail.com>
2024-09-19 22:38:57 +0200

    register adapter

--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -10,7 +10,7 @@ class AddNoteButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: MediaQuery.of(context).viewInsets,
-      child: SingleChildScrollView(
+      child: const SingleChildScrollView(
         child: AddNoteForm(),
       ),
     );
